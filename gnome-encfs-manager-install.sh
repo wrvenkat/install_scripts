9bb842510194469f9b821d63e16444c702bbc8f1
W.R.Venkat <wrvenkat@gmail.com>
2017-02-02 22:25:07 -0500

    Added install script for steam. Fixed some minor bugs with encfs install script.

--- gnome-encfs-manager-install.sh
+++ gnome-encfs-manager-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Autoaccepts the security info and then installs gnome-encfs-manager
-echo encfs encfs/security-information select true | sudo debconf-set-selections
+printf "encfs encfs/security-information select true" | sudo debconf-set-selections
 sudo add-apt-repository -y ppa:gencfsm/ppa
 sudo apt-get update
 sudo apt-get install -y gnome-encfs-manager
