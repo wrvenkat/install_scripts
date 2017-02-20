c5cbbe0c3ded6f1e3f748406886da761068351f9
W.R.Venkat <wrvenkat@gmail.com>
2017-02-20 09:32:25 -0500

    Upadated scucloud script.

--- scudcloud-install.sh
+++ scudcloud-install.sh
@@ -15,7 +15,7 @@ fi
 
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
 sudo apt-get update
-if ! sudo apt-get install scudcloud; then
+if ! sudo apt-get install -y scudcloud; then
     printf "Error installing scudcloud. Exiting\n"
     exit 1
 fi
