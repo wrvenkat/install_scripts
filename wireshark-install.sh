57521bec6268d30af5cc56d0f26287fa525753cb
W.R.Venkat <wrvenkat@gmail.com>
2018-03-25 17:42:03 -0400

    Modified wireshark install script.

--- wireshark-install.sh
+++ wireshark-install.sh
@@ -5,14 +5,21 @@ if ! sudo apt-get update; then
     exit 1
 fi
 
+#allow non-root for wireshark by setting debconf selections
+if ! echo wireshark-common wireshark-common/install-setuid select true | sudo debconf-set-selections; then
+    printf "Adding non-root capture failed. Exiting\n"
+    exit 1
+fi
+
+#install
 if ! sudo apt-get install -y --allow-unauthenticated wireshark; then
     printf "Wireshark installation failed. Exiting.\n"
     exit 1
 fi
 
-#allow non-root for wireshark by setting debconf selections; adding a wireshark group and adding the current user to that
-if ! echo wireshark-common wireshark-common/install-setuid select true | sudo debconf-set-selections && sudo groupadd wireshark && sudo usermod -a -G wireshark $USER; then
-    printf "Adding non-root capture failed. Exiting\n"
+#add the current user to wireshark group
+if ! sudo usermod -a -G wireshark $USER; then
+    printf "Adding to wireshark group failed. Exiting\n"
     exit 1
 fi
 
