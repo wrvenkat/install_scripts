d109918c5b4a853d5fc34018bf68b98ee7b9fcf6
W.R.Venkat <wrvenkat@gmail.com>
2018-03-26 08:21:34 -0400

    Fixed jtool folder.

--- jtool-install.sh
+++ jtool-install.sh
@@ -3,6 +3,7 @@
 #See http://www.newosxbook.com/tools/jtool.html
 
 URL="http://www.newosxbook.com/tools/jtool.tar"
+target_dir=~/Downloads/
 
 #check for wget
 if ! type ; then
@@ -14,23 +15,23 @@ if ! type ; then
 fi
 
 #download
-if ! wget -q "$URL" -O ~/Downloads/jtool.tar; then
+if ! wget -q "$URL" -O "$target_dir"jtool.tar; then
     printf "jtoll download failed. Exiting.\n"
     exit 1    
 fi
 
 #extract jtool
-if ! [ -d ~/Downloads/jtool/ ]; then
-    mkdir ~/Downloads/jtool;
+if ! [ -d "$target_dir"jtool/ ]; then
+    mkdir "$target_dir"jtool;
 fi
 
-if ! tar -xvf ~/Downloads/jtool.tar --directory ~/Downloads/jtool/; then
+if ! tar -xvf "$target_dir"jtool.tar --directory "$target_dir"jtool/; then
     printf "jtool extract failed. Exiting\n";
     exit 1
 fi
 
 #rename and move to /bin.
-if ! (cd ~/Downloads/jtool && mv ~/Downloads/jtool ~/Downloads/jtool1 && mv ~/Downloads/jtool.ELF64 ~/Downloads/jtool && sudo cp ~/Downloads/jtool /bin/); then
+if ! (cd "$target_dir"jtool && mv "$target_dir"jtool "$target_dir"jtool1 && mv "$target_dir"jtool.ELF64 "$target_dir"jtool && sudo cp "$target_dir"jtool /bin/); then
     printf "Failed to copy to /bin. Exiting\n"
     exit 1
 fi
